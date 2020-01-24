66647b0e856053bf32c130978cd55cc96fcbb8bd
Micah Rogers <micahrogers@hotmail.com>
2020-01-24 16:58:30 -0700

    more testing

--- web/prove3.php
+++ web/prove3.php
@@ -26,6 +26,7 @@ $_SESSION[cart] = array();
 
   function addToCart(Item $newItem)
   {  
+alert("Hello! I am an alert box!!");
     var found = false;
     foreach ($_SESSION[cart] as $cartItem)
     {
@@ -40,7 +41,6 @@ $_SESSION[cart] = array();
       $_SESSION[cart].push($newItem);
     }
    echo $_SESSION[cart];
-alert("Hello! I am an alert box!!");
   }
 
 </script>
@@ -57,15 +57,15 @@ alert("Hello! I am an alert box!!");
 <div class="row">
   <div class="col-4 col-s-6"><img class="wayofkings" src="wayofkings.png" alt="The Way of Kings">
     <p>The Way of Kings</p>
-    <button onclick="addToCart(new Item ("The Way of Kings", 9.99)">Add to Cart</button>
+    <button onclick="addToCart(new Item ("The Way of Kings", 9.99))">Add to Cart</button>
   </div>
   <div class="col-4 col-s-6"><img class="pingpong" src="pingpong.png" alt="Ping Pong">
     <p>Ping Pong Table: Two paddles and one ball included</p>
-    <button onclick="addToCart(new Item ("Ping Ping Table", 99.99)">Add to Cart</button>
+    <button onclick="addToCart(new Item ("Ping Ping Table", 99.99))">Add to Cart</button>
   </div>
   <div class="col-4 col-s-6"><img class="candyland" src="boardgame.png" alt="Board Games">
     <p>Candy Land</p>
-    <button onclick="addToCart(new Item ("Candyland", 14.99)">Add to Cart</button>
+    <button onclick="addToCart(new Item ("Candyland", 14.99))">Add to Cart</button>
   </div>  
 </div>
 </body>
