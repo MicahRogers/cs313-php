d101015b74d335e64386659ec7445568305b9f6b
Micah Rogers <micahrogers@hotmail.com>
2020-01-24 19:34:26 -0700

    more testing

--- web/prove3.php
+++ web/prove3.php
@@ -45,6 +45,19 @@ function myFunction() {
 <body>
 <button onclick="myFunction()">Try it</button>
 
-
+<div class="row">
+  <div class="col-4 col-s-6"><img class="wayofkings" src="wayofkings.png" alt="The Way of Kings">
+    <p>The Way of Kings</p>
+    <button type="button" onclick="addToCart(<script>var item = { name: "The Way of Kings", price : 9.99, quantity : 1};)</script>>Add to Cart</button>
+  </div>
+  <div class="col-4 col-s-6"><img class="pingpong" src="pingpong.png" alt="Ping Pong">
+    <p>Ping Pong Table: Two paddles and one ball included</p>
+    <button type="button" onclick="addToCart(<script>var item = { name: "Ping Pong Table", price : 99.99, quantity : 1};)</script>">Add to Cart</button>
+  </div>
+  <div class="col-4 col-s-6"><img class="candyland" src="boardgame.png" alt="Board Games">
+    <p>Candy Land</p>
+    <button type="button" onclick="addToCart(<script>var item = { name: "Candy Land", price : 14.99, quantity : 1};)</script>>Add to Cart</button>
+  </div>  
+</div>
 </body>
 </html>
