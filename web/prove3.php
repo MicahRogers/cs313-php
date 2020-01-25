a1d2c4816b312c21494c4ade07fd24bfb319aabf
Micah Rogers <micahrogers@hotmail.com>
2020-01-24 20:15:22 -0700

    more testing

--- web/prove3.php
+++ web/prove3.php
@@ -20,20 +20,7 @@ function myFunction() {
   function addToCart(newItem)
   {  
     alert("Hello! I am an alert box!!");
-    var found = false;
-    foreach (<?php $_SESSION["cart"]?> as cartItem)
-    {
-      if (newItem.name == cartItem.name)
-      {
-        found = true;
-        cartItem.quantity++;
-      }
-    }   
-    if (!found)
-    {
-      <<?php $_SESSION["cart"]?>.push(newItem);
-    }
-    <?php echo $_SESSION["cart"];?>
+
   }
 
 
