8dcc796ae275383b34b7430fc3b4054fb66c4867
Micah Rogers <micahrogers@hotmail.com>
2020-01-24 19:36:20 -0700

    more testing

--- web/prove3.php
+++ web/prove3.php
@@ -48,15 +48,15 @@ function myFunction() {
 <div class="row">
   <div class="col-4 col-s-6"><img class="wayofkings" src="wayofkings.png" alt="The Way of Kings">
     <p>The Way of Kings</p>
-    <button type="button" onclick="addToCart(<script>var item = { name: "The Way of Kings", price : 9.99, quantity : 1};)</script>>Add to Cart</button>
+    <button type="button" onclick="addToCart(<script>var item = { name: "The Way of Kings", price : 9.99, quantity : 1};)</script>Add to Cart</button>
   </div>
   <div class="col-4 col-s-6"><img class="pingpong" src="pingpong.png" alt="Ping Pong">
     <p>Ping Pong Table: Two paddles and one ball included</p>
-    <button type="button" onclick="addToCart(<script>var item = { name: "Ping Pong Table", price : 99.99, quantity : 1};)</script>">Add to Cart</button>
+    <button type="button" onclick="addToCart(<script>var item = { name: "Ping Pong Table", price : 99.99, quantity : 1};)</script>Add to Cart</button>
   </div>
   <div class="col-4 col-s-6"><img class="candyland" src="boardgame.png" alt="Board Games">
     <p>Candy Land</p>
-    <button type="button" onclick="addToCart(<script>var item = { name: "Candy Land", price : 14.99, quantity : 1};)</script>>Add to Cart</button>
+    <button type="button" onclick="addToCart(<script>var item = { name: "Candy Land", price : 14.99, quantity : 1};)</script>Add to Cart</button>
   </div>  
 </div>
 </body>
