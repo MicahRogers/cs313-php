50abb7be20aff14c3444f764fccc6278c029ee88
Micah Rogers <micahrogers@hotmail.com>
2020-02-15 14:52:14 -0700

    test

--- web/favorite_boardgame_adding.php
+++ web/favorite_boardgame_adding.php
@@ -25,8 +25,50 @@
  echo "max=$max_players\n";
  echo "coop or comp=$coop_or_comp\n";
 
-require("dbConnect.php");
-$db = get_db();
+//require("dbConnect.php");
+	$db = NULL;
+
+	try {
+		// default Heroku Postgres configuration URL
+		$dbUrl = getenv('DATABASE_URL');
+
+		if (!isset($dbUrl) || empty($dbUrl)) {
+			// example localhost configuration URL with user: "ta_user", password: "ta_pass"
+			// and a database called "scripture_ta"
+			$dbUrl = "postgres://ta_user:ta_pass@localhost:5432/scripture_ta";
+
+			// NOTE: It is not great to put this sensitive information right
+			// here in a file that gets committed to version control. It's not
+			// as bad as putting your Heroku user and password here, but still
+			// not ideal.
+			
+			// It would be better to put your local connection information
+			// into an environment variable on your local computer. That way
+			// it would work consistently regardless of whether the application
+			// were running locally or at heroku.
+		}
+
+		// Get the various parts of the DB Connection from the URL
+		$dbopts = parse_url($dbUrl);
+
+		$dbHost = $dbopts["host"];
+		$dbPort = $dbopts["port"];
+		$dbUser = $dbopts["user"];
+		$dbPassword = $dbopts["pass"];
+		$dbName = ltrim($dbopts["path"],'/');
+
+		// Create the PDO connection
+		$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+		// this line makes PDO give us an exception when there are problems, and can be very helpful in debugging!
+		$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+	}
+	catch (PDOException $ex) {
+		// If this were in production, you would not want to echo
+		// the details of the exception.
+		echo "Error connecting to DB. Details: $ex";
+		die();
+	}
 
 	$query = 'INSERT INTO boardgames(boardgame_name, boardgame_min_players, boardgame_max_players,
 		  boardgame_coop_or_comp, publisher_id) VALUES(:name, :min, :max, :coop_or_comp, :publisher_id)';
@@ -42,7 +84,23 @@ $db = get_db();
 
 	$statement->execute();
 
+	// get the new id
+	//$scriptureId = $db->lastInsertId("scripture_id_seq");
 
+	// Now go through each topic id in the list from the user's checkboxes
+	//foreach ($topicIds as $topicId)
+//	{
+//		echo "ScriptureId: $scriptureId, topicId: $topicId";
+
+//		// Again, first prepare the statement
+//		$statement = $db->prepare('INSERT INTO scripture_topic(scriptureId, topicId) VALUES(:scriptureId, :topicId)');
+//
+//		// Then, bind the values
+//		$statement->bindValue(':scriptureId', $scriptureId);
+//		$statement->bindValue(':topicId', $topicId);
+
+//		$statement->execute();
+//	}
 
 
 ?>
