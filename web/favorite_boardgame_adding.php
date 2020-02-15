20e2c8868389801b70b4107c11be8b17eb5c6603
Micah Rogers <micahrogers@hotmail.com>
2020-02-15 16:14:55 -0700

    test

--- web/favorite_boardgame_adding.php
+++ web/favorite_boardgame_adding.php
@@ -46,12 +46,6 @@
 // For debugging purposes, you might include some echo statements like this
 // and then not automatically redirect until you have everything working.
 
- echo "name=$name\n";
- echo "publisher=$publisher\n";
- echo "min=$min_players\n";
- echo "max=$max_players\n";
- echo "coop or comp=$coop_or_comp\n";
-
 //require("dbConnect.php");
 
 
