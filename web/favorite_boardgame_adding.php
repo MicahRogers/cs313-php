c434164167de3e6e77c877daee07335659129951
Micah Rogers <micahrogers@hotmail.com>
2020-02-15 14:49:11 -0700

    test

--- web/favorite_boardgame_adding.php
+++ web/favorite_boardgame_adding.php
@@ -28,6 +28,21 @@
 require("dbConnect.php");
 $db = get_db();
 
+	$query = 'INSERT INTO boardgames(boardgame_name, boardgame_min_players, boardgame_max_players,
+		  boardgame_coop_or_comp, publisher_id) VALUES(:name, :min, :max, :coop_or_comp, :publisher_id)';
+	$statement = $db->prepare($query);
+
+	// Now we bind the values to the placeholders. This does some nice things
+	// including sanitizing the input with regard to sql commands.
+	$statement->bindValue(':book', $name);
+	$statement->bindValue(':chapter', $min_players);
+	$statement->bindValue(':verse', max=$max_players);
+	$statement->bindValue(':content', $coop_or_comp);
+        $statement->bindValue(':content', $publisher_id);
+
+	$statement->execute();
+
+
 
 
 ?>
