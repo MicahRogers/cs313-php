23c69aad248d05eebc5dbc2d95740175d22059d5
Micah Rogers <micahrogers@hotmail.com>
2020-02-19 15:45:41 -0700

    test

---
+++ group/signup.php
@@ -0,0 +1,56 @@
+<?php
+  session_start();
+
+  $dbUrl = getenv('DATABASE_URL');
+
+  if (empty($dbUrl)) {
+   // example localhost configuration URL with postgres username and a database called cs313db
+   $dbUrl = "postgres://postgres:password@localhost:5432/cs313db";
+  }
+  
+  $dbopts = parse_url($dbUrl);
+  
+  $dbHost = $dbopts["host"];
+  $dbPort = $dbopts["port"];
+  $dbUser = $dbopts["user"];
+  $dbPassword = $dbopts["pass"];
+  $dbName = ltrim($dbopts["path"],'/');
+  
+  //print "<p>pgsql:host=$dbHost;port=$dbPort;dbname=$dbName</p>\n\n";
+  
+  try {
+   $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+  }
+  catch (PDOException $ex) {
+   print "<p>error: $ex->getMessage() </p>\n\n";
+   die();
+  }
+
+$newURL = 'login.php';
+header('Location: ' . $newURL);
+die();
+?>
+
+ 
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" type="text/css" href="homepage.css">
+
+</head>
+<header>
+
+</header>
+<body>
+
+<form method="POST">
+
+<input name="username" type="text"><br>
+<input name="password" type="password">
+<button type='submit'>
+</form>
+
+</body>
+
+</html>
