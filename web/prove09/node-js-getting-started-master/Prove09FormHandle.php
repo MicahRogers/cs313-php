161dd48ca89dfa833be7a278ac3415b0889e774e
Micah Rogers <micahrogers@hotmail.com>
2020-03-05 14:44:51 -0700

    test

--- web/prove09/node-js-getting-started-master/Prove09FormHandle.php
+++ web/prove09/node-js-getting-started-master/Prove09FormHandle.php
@@ -1,9 +1,41 @@
-<?php
-$weight = $_POST[weight];
-$shipping = $_POST[shipping];
-echo $weight;
-echo $shipping;
-?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" type="text/css" href="homepage.css">
+<title>Micah homepage</title>
+</head>
+<header>
+  <a href="assignments.html">
+  <p>|Assignments|</p>
+  </a>
+
+  <h1>Some of my Favorite things</h1>
+</header>
+<body>
+<div class="row">
+  <div class="col-4 col-s-6"><img class="wayofkings" src="wayofkings.png" alt="The Way of Kings">
+    <p>The Stormlight Archives: "Roshar is a world of stone and storms. Uncanny tempests of incredible power sweep across the rocky terrain so frequently
+ that they have shaped ecology and civilization alike. Cities are built only where the topography offers shelter.
+It has been centuries since the fall of the ten consecrated orders known as the Knights Radiant, but their
+ Shardblades and Shardplate remain: mystical swords and suits of armor that transform ordinary men into 
+near-invincible warriors. Men trade kingdoms for Shardblades. Wars were fought for them, and won by them."
+    </p>
+  </div>
+  <div class="col-4 col-s-6"><img class="pingpong" src="pingpong.png" alt="Ping Pong"><p>Ping Pong: My parents got a ping pong table when I was young. I mostly played with 
+my older brother. A kew part of playing against my brother is the good-natured trash talk. It is pretty much non-stop and some people who overhear us probaby think we hate 
+each other</p></div>
+  <div class="col-4 col-s-6"><img src="boardgame.png" alt="Board Games"> <p>Board Games: I enjoy getting together with friends to play board games.
+Since i can tend to be a little competetive, I prefer to play cooperative board games. Some of my favorites are Mansions of Madness, 
+Spirit Island, and Pandemic</p></div>  
+</div>
+</body>
+<footer>
+ <?php
+    echo "The time is " . date("h:i:sa");
+  ?>
+</footer
+</html>
 
 
 
